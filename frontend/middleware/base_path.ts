f5b7b8f08380ccdc7662a91e877e8281fa12518b
GodofBattle <GodofBattle@users.noreply.github.com>
2022-04-12 11:37:58 +0900

    redirect /

--- frontend/middleware/base_path.ts
+++ frontend/middleware/base_path.ts
@@ -1,10 +1,23 @@
 import { Middleware, Context } from "@nuxt/types";
+import { decode } from 'jsonwebtoken';
 
 const base_path: Middleware = async ({ $apolloHelpers, route, redirect, store }: Context) => {
     const token = $apolloHelpers.getToken();
 
     if (!token && route?.path === '/') {
         redirect({ name: 'login' });
+    } else if (token && route?.path === '/') {
+        try {
+            const { iDCIM: { roles } }: any = decode(token);
+
+            if (roles === 'PERM01') {
+                redirect({ path: '/icomer/code' });
+            } else if (roles === 'PERM02') {
+                redirect({ path: '/admin/home' });
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
 
